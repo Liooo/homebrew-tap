1dcbbfc750d9d25a6638b2a819058e85184a6147
Liooo <ryoyamada3@gmail.com>
2026-01-25 01:24:37 +0900

    Update just-dictate sha256 for v0.1.0

--- Casks/just-dictate.rb
+++ Casks/just-dictate.rb
@@ -1,6 +1,6 @@
 cask "just-dictate" do
   version "0.1.0"
-  sha256 "REPLACE_ME"
+  sha256 "8c19950a290d2cbb962d92f1820111e4c7f71cf76dc3ef7f0e5296afe03c3d48"
 
   url "https://github.com/Liooo/just-dictate/releases/download/v#{version}/Just-Dictate.zip"
   name "Just Dictate"
