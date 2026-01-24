dd4c6215f7e200e85a5b5a5787a0b413fabad4a3
Liooo <ryoyamada3@gmail.com>
2026-01-25 01:12:53 +0900

    Add just-dictate cask

---
+++ Casks/just-dictate.rb
@@ -0,0 +1,11 @@
+cask "just-dictate" do
+  version "0.1.0"
+  sha256 "REPLACE_ME"
+
+  url "https://github.com/Liooo/just-dictate/releases/download/v#{version}/Just-Dictate.zip"
+  name "Just Dictate"
+  desc "Lightweight macOS menu bar dictation app"
+  homepage "https://github.com/Liooo/just-dictate"
+
+  app "Just Dictate.app"
+end
